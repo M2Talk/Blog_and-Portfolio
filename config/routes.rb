850308da12f176cdd60969edcc4e3c280a80908f
M2Talk <mtalkowski1903@gmail.com>
2022-03-31 10:41:15 +0200

    Implemented image defaults for portfolio items

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :portfolios, except: [:show]
-  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  resources :portfolios
+
   get 'angular-items', to: 'portfolios#angular'
   root to: 'pages#home'
   get 'about-me', to: 'pages#about'
