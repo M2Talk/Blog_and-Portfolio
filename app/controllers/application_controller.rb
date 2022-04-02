1477876c852252dfa37ff582ab1a2b5190373930
M2Talk <mtalkowski1903@gmail.com>
2022-04-02 11:10:16 +0200

    Refactored application controller to include concern for source feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_action :set_source
-
-  def set_source
-    session[:source] = params[:q] if params[:q]
-  end
+  include SessionSource
 end
